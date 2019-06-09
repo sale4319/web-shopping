eb30a6c54f318b06a7d074c631b68eb9b103628f
ASUS <ASUS@DESKTOP-J12S1DT>
2019-06-09 22:31:48 +0200

    Updating DB sample

--- webshoppingbackend/databaseQueries.sql
+++ webshoppingbackend/databaseQueries.sql
@@ -32,11 +32,11 @@ VALUES('Sale', 'Saki', 'ADMIN', true, '$2a$10$d2z0Oy3EgLK1IY3w/QRHHe/2kSH6JEB23I
 
 INSERT INTO user_detail
 (first_name, last_name, role, enabled, password, email, contact_number)
-VALUES('Ica', 'Dandarica', 'SUPPLIER', true, '$2b$10$05pi8bqK2uPIaDgi7G.Y0ugufr60DMDv/q6tIcXi8K26A4WjV2Wca', 'i@gmail.com', '1111111111');
+VALUES('Ica', 'Dandarica', 'USER', true, '$2a$10$/turYA6ZuIKnyHJo271sMe39OfnA4roxLI0awKnrFHICJEscrNGA2', 'i@gmail.com', '1111111111');
 
 INSERT INTO user_detail
 (first_name, last_name, role, enabled, password, email, contact_number)
-VALUES('Maca', 'Katosav', 'SUPPLIER', true, '$2b$10$ar.tKo/We9BRgpCO5YIynegM00ZTYL5HQ65Deh8eQKWY1EIMOoeDy', 'k@gmail.com', '1111111111');
+VALUES('Maca', 'Katosav', 'SUPPLIER', true, '$2a$10$/turYA6ZuIKnyHJo271sMe39OfnA4roxLI0awKnrFHICJEscrNGA2', 'k@gmail.com', '1111111111');
 
 CREATE TABLE product (
 	id IDENTITY,
@@ -58,19 +58,49 @@ CREATE TABLE product (
 );
 
 INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id, purchases, views)
-VALUES ('PRDABC123DEFX', 'iPhone X', 'apple', 'This phone is expensive.', '5000', '5', 'true', 3,2,0,0);
+VALUES ('PRDABC123DEFX', 'iPhone 5s', 'apple', 'This is one of the best phone available in the market right now!', 18000, 5, true, 3, 2, 0, 0 );
 
 INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id, purchases, views)
-VALUES ('PRDACC123DEFX', 'Samsung s11', 'samsung', 'This phone is old by now.', '4000', '2', 'true', 3,3,0,0);
+VALUES ('PRDDEF123DEFX', 'Samsung s7', 'samsung', 'A smart phone by samsung!', 32000, 2, true, 3, 3, 0, 0 );
 
 INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id, purchases, views)
-VALUES ('PRDADC123DEFX', 'Google Nexus', 'google', 'This was good stuff.', '3000', '5', 'true', 3,2,0,0);
+VALUES ('PRDPQR123WGTX', 'Google Pixel', 'google', 'This is one of the best android smart phone available in the market right now!', 57000, 5, true, 3, 2, 0, 0 );
 
 INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id, purchases, views)
-VALUES ('PRDAEC123DEFX', 'Macbook Pro', 'apple', 'This is expensive stuff.', '6000', '3', 'true', 1,2,0,0);
+VALUES ('PRDMNO123PQRX', ' Macbook Pro', 'apple', 'This is one of the best laptops available in the market right now!', 54000, 3, true, 1, 2, 0, 0 );
 
 INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id, purchases, views)
-VALUES ('PRDAFC123DEFX', 'Asus X550', 'asus', 'This phone is my laptop.', '1000', '5', 'true', 1,3,0,0);
+VALUES ('PRDABCXYZDEFX', 'Dell Latitude E6510', 'dell', 'This is one of the best laptop series from dell that can be used!', 48000, 5, true, 1, 3, 0, 0 );
+
+
+CREATE TABLE address (
+	id IDENTITY,
+	user_id int,
+	address_line_one VARCHAR(100),
+	address_line_two VARCHAR(100),
+	city VARCHAR(20),
+	state VARCHAR(20),
+	country VARCHAR(20),
+	postal_code VARCHAR(10),
+	is_billing BOOLEAN,
+	is_shipping BOOLEAN,
+	CONSTRAINT fk_address_user_id FOREIGN KEY (user_id ) REFERENCES user_detail (id),
+	CONSTRAINT pk_address_id PRIMARY KEY (id)
+);
+
+INSERT INTO address( user_id, address_line_one, address_line_two, city, state, country, postal_code, is_billing, is_shipping) 
+VALUES (1, 'Cara Musana', 'Staarog', 'Bujdo', 'RS', 'BA', '76688', true, false );
+
+CREATE TABLE cart (
+	id IDENTITY,
+	user_id int,
+	grand_total DECIMAL(10,2),
+	cart_lines int,
+	CONSTRAINT fk_cart_user_id FOREIGN KEY (user_id ) REFERENCES user_detail (id),
+	CONSTRAINT pk_cart_id PRIMARY KEY (id)
+);
+
+INSERT INTO cart (user_id, grand_total, cart_lines) VALUES (4, 0, 0);
 
 CREATE TABLE cart_line(
 	id IDENTITY,
