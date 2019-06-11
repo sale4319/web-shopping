8d8ac0841d6f111d2e9967ca7ef7dd42fd27d94f
Aleksandar Stojanovic <sale4319@gmail.com>
2019-06-11 21:16:04 +0200

    Updating cart status accurately

--- webshopping/src/main/java/com/sale/webshopping/controller/CartController.java
+++ webshopping/src/main/java/com/sale/webshopping/controller/CartController.java
@@ -2,7 +2,9 @@ package com.sale.webshopping.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sale.webshopping.service.CartService;
@@ -15,15 +17,32 @@ public class CartController {
 	private CartService cartService;
 	
 	@RequestMapping("/show")
-	public ModelAndView showCart() {
-		
+	public ModelAndView showCart(@RequestParam(name="result", required=false) String result) {		
 		ModelAndView mv = new ModelAndView("page");
+		
+		if(result!=null) {
+			switch(result){
+				case "updated":
+					mv.addObject("message", "Cartline has been updated successfully!");
+					break;
+				case "error":
+					mv.addObject("message", "Something went wrong!");
+					break;
+			}
+		}
 		
 		mv.addObject("title", "User Cart");
 		mv.addObject("userClickShowCart", true);
 		mv.addObject("cartLines", cartService.getCartLines());
 		
-		return mv;
+		return mv;		
+		}
+	
+	@RequestMapping("/{cartLineId}/update")
+	public String updateCart(@PathVariable int cartLineId, @RequestParam int count) {
 		
+		String response = cartService.updateCartLine(cartLineId, count);
+		return "redirect:/cart/show?" + response;
 	}
+
 }
--- webshopping/src/main/java/com/sale/webshopping/service/CartService.java
+++ webshopping/src/main/java/com/sale/webshopping/service/CartService.java
@@ -11,24 +11,53 @@ import com.sale.webshopping.model.UserModel;
 import com.sale.webshoppingbackend.dao.CartLineDAO;
 import com.sale.webshoppingbackend.dto.Cart;
 import com.sale.webshoppingbackend.dto.CartLine;
+import com.sale.webshoppingbackend.dto.Product;
 
 @Service("cartService")
 public class CartService {
 
 	@Autowired
 	private CartLineDAO cartLineDAO;
-	
+
 	@Autowired
 	private HttpSession session;
-	
+
 	// returns the cart of the user who is logged in
 	private Cart getCart() {
-		return ((UserModel)session.getAttribute("userModel")).getCart();
+		return ((UserModel) session.getAttribute("userModel")).getCart();
 	}
-	
+
 	// returns the entire cart lines
-	public List<CartLine> getCartLines(){		
+	public List<CartLine> getCartLines() {
 		return cartLineDAO.list(this.getCart().getId());
 	}
-	
+
+	public String updateCartLine(int cartLineId, int count) {
+
+		// fetch the cart line
+		CartLine cartLine = cartLineDAO.get(cartLineId);
+		
+		if(cartLine == null) {
+			
+			return "result=error";
+		}
+		else {
+			Product product = cartLine.getProduct();
+			
+			double oldTotal = cartLine.getTotal();
+			
+			if(product.getQuantity() <= count) {
+				count = product.getQuantity();
+			}
+			cartLine.setProductCount(count);
+			cartLine.setBuyingPrice(product.getUnitPrice());			
+			cartLine.setTotal(product.getUnitPrice() * count);			
+			cartLineDAO.update(cartLine);			
+			Cart cart = this.getCart();
+			cart.setGrandTotal(cart.getGrandTotal() - oldTotal + cartLine.getTotal());			
+			cartLineDAO.updateCart(cart);			
+			return "result=updated";
+		}				
+	}
+
 }
